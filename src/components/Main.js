bc7278022fe9a3ec8a4a684656d851cc79ade3eb
Robert Mrowiec <rmrowiec@icloud.com>
2018-10-31 20:06:12 +0100

    created description page with 2 links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 export default class Main extends Component {
     render() {
         return (
-            <div className='mainDescription'>
-                ELO HERE
+            <div style={{height: '9vh'}}>
+
+                <div className="App">
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
+                    <Navbar />
+                </div>
+        
+                <Sidebar />
             </div>
         )
     }
---
+++ src/components/MyProfile.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class MyProfile extends Component {
+    render() {
+        console.log(123);
+        return(
+            <div className='mainDescription'>
+                Profile page works
+            </div>
+        )
+    }
+}
