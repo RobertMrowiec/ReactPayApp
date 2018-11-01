706041ae4180c43826dd9dd47d8a282942162b54
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-02 00:17:23 +0100

    update CSS

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,43 +1,51 @@
 import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default class Sidebar extends Component {
     render() {
         return (
-          <div className="navbar-default navbar-fixed-side sidebar">
+          <div className="navbar-fixed-side sidebar">
             <div className='sidebarList'>
               <ul>
-                <Link to="/app/dashboard"> 
-                  <i className="fas fa-home fa-2x"></i><br/>
-                  Pulpit
-                </Link>
+                <NavLink className="hover-container" activeClassName="dot" to="/app/dashboard"> 
+                  <div className='test'>
+                    <i className="fas fa-home fa-2x"></i><br/>
+                  </div>
+                  <p style={{paddingTop: '10px'}}> Pulpit </p>
+                </NavLink>
               </ul>
             </div>
             
             <div className='sidebarList'>
               <ul>
-                <Link to='/app/users'>
-                  <i className="fas fa-users fa-2x"></i> <br/>
-                  Pracownicy
-                </Link>
+                <NavLink className="hover-container" activeClassName="dot" to='/app/users'>
+                  <div className='test'>
+                    <i className="fas fa-users fa-2x"></i> <br/>
+                  </div>
+                  <p style={{paddingTop: '10px'}}> Pracownicy </p>
+                </NavLink>
               </ul>
             </div>
 
             <div className='sidebarList'>
               <ul>
-                <Link to="/app/projects">
-                  <i className="fas fa-clipboard-list fa-2x"></i> <br/>
-                  Projekty
-                </Link>
+                <NavLink className="hover-container" activeClassName="dot" to="/app/projects">
+                  <div className='test'>
+                    <i className="fas fa-clipboard-list fa-2x"></i> <br/>
+                  </div>
+                  <p style={{paddingTop: '10px'}}> Projekty </p>
+                </NavLink>
               </ul>
             </div>
 
             <div className='sidebarList'>
               <ul>
-                <Link to="/app/salaries">
-                  <i className="fas fa-wallet fa-2x"></i><br/>
-                  Wypłaty
-                </Link>
+                <NavLink className="hover-container" activeClassName="dot" to="/app/salaries">
+                  <div className='test'>
+                    <i className="fas fa-wallet fa-2x"></i><br/>
+                  </div>
+                  <p style={{paddingTop: '10px'}}> Wypłaty </p>
+                </NavLink>
               </ul>
             </div>
           </div>
