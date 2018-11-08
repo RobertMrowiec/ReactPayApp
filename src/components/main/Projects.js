16c6e872e001b6d916c5cb3d296e8206d11eb60e
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-08 19:24:08 +0100

    update project page

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react'
-import { checkToken } from '../Common';
+import { checkToken, checkStatus } from '../Common';
 import Loader from '../navigation/Loader'
-import bpcLogo from './bpc.png'
 import {Link} from 'react-router-dom'
+import bpcLogo from './bpc.png'
 
 export default class Projects extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            projects: [],
+            loading: true
+        }
     }
 
     componentDidMount() {
         checkToken(this.props.history)
-    }
 
+        fetch('http://localhost:8002/projects', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }).then(checkStatus)
+        .then(x => x.json())
+        .then(data => this.setState({projects: data, loading: false}))
+        .catch(err => console.log(err))
+    }
     render() {
         const { loading } = this.state
         
@@ -23,15 +35,20 @@ export default class Projects extends Component {
 
         return (
             <div className='mainDescription'>
-                <div className='card' style={{width: '18rem'}}>
-                    <img alt='' className='card-img-top' src={bpcLogo}/>
-                    <div className='card-body'>
-                        <h5 className='card-title'>Card title</h5>
-                        <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <Link to='/projects/' className='btn btn-primary'> Go somewhere </Link>
-                    </div>
+                <div className='projectsCards'>
+                    {this.state.projects.map((obj, i) => {
+                        return (
+                            <div className='card' style={{width: '18rem'}}>
+                                <div className='card-body'>
+                                    <img alt='' className='card-img-top' src={obj.photo}/>
+                                    <h5 className='card-title'>{obj.name}</h5>
+                                    <p className='card-text'>{obj.photo}</p>
+                                    <Link to='/projects/' className='btn btn-primary'> Check project </Link>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
-
             </div>
         )
     }
